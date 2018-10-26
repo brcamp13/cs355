16c0c76560403d1720faa2af7279c756dc00a0ec
Brandon.Campbell <brcamp13@yahoo.com>
2018-10-25 17:28:37 -0700

    She messed up the percentages, but very little left

--- assignment4Part1.py
+++ assignment4Part1.py
@@ -44,8 +44,8 @@ def dict_pop():
         print('Can\'t pop as there are no items in the dict stack!')
         return
     else:
-        return_value = op_stack[-1]
-        del op_stack[-1]
+        return_value = dict_stack[-1]
+        del dict_stack[-1]
         return return_value
 
 
@@ -68,8 +68,12 @@ def dict_push(d):
 def define(name, value):
 
     global dict_stack
-    dict_to_push = {name: value}
-    dict_stack[-1].update(dict_to_push)
+
+    if name[0] != '/':
+        print('The name does not start with \' / \'')
+    else:
+        dict_to_push = {name: value}
+        dict_stack[-1].update(dict_to_push)
 
 
 # add name:value to the top dictionary in the dictionary stack. (Keep the ‘/’ in
@@ -320,9 +324,156 @@ def get():
 # Boolean operators: define the boolean operators psAnd, psOr, psNot
 # Remember that these take boolean operands only. Anything else is an error
 
+def ps_and():
+
+    global op_stack
+
+    # Checks if there are enough operands in the stack
+    if len(op_stack) >= 2:
+
+        val_1 = op_pop()
+        # Checks if the first value popped is an integer. If so, then it continues
+        if type(val_1) == bool:
+
+            val_2 = op_pop()
+            # If both are integers, then perform the operation. Otherwise, push all values back to stack
+            if type(val_2) == bool:
+                return_val = val_1 and val_2
+                op_push(return_val)
+            else:
+                op_push(val_2)
+                op_push(val_1)
+                print('One or more of the values was not an integer!')
+        else:
+            op_push(val_1)
+            print('One or more of the values was not an integer')
+    else:
+        print('Not enough values in stack to perform the operation!')
+
+
+def ps_or():
+
+    global op_stack
+
+    # Checks if there are enough operands in the stack
+    if len(op_stack) >= 2:
+
+        val_1 = op_pop()
+        # Checks if the first value popped is an integer. If so, then it continues
+        if type(val_1) == bool:
+
+            val_2 = op_pop()
+            # If both are integers, then perform the operation. Otherwise, push all values back to stack
+            if type(val_2) == bool:
+                return_val = val_1 or val_2
+                op_push(return_val)
+            else:
+                op_push(val_2)
+                op_push(val_1)
+                print('One or more of the values was not an integer!')
+        else:
+            op_push(val_1)
+            print('One or more of the values was not an integer')
+    else:
+        print('Not enough values in stack to perform the operation!')
+
+
+def ps_not():
+
+    global op_stack
+
+    # Checks if there are enough operands in the stack
+    if len(op_stack) >= 1:
+
+        val_1 = op_pop()
+        # Checks if the first value popped is an integer. If so, then it continues
+        if type(val_1) == bool:
+            return_value = not val_1
+            op_push(return_value)
+        else:
+            op_push(val_1)
+            print('The value at the top of the stack was not a bool!')
+    else:
+        print('Not enough values in stack to perform the operation!')
+
 # --------------------------- 25% -------------------------------------
 # Define the stack manipulation and print operators: dup, exch, pop, copy,
 # clear, stack
+
+
+def dup():
+
+    global op_stack
+
+    # Check if the stack is empty
+    if len(op_stack) >= 1:
+
+        # Get the top value and then push it to the stack
+        top_val = op_stack[-1]
+        op_push(top_val)
+
+    else:
+        print("Cannot perform the operation as the stack is empty!")
+
+
+def exch():
+
+    global op_stack
+
+    # Check if there are at least 2 values in the stack
+    if len(op_stack) >= 2:
+
+        val_1 = op_pop()
+        val_2 = op_pop()
+
+        # Just reverse the order of them by pushing in different order
+        op_push(val_1)
+        op_push(val_2)
+
+    else:
+        print("There are not enough values in the stack to perform this operation")
+
+
+def pop():
+    pass
+    # Is this not already accomplished with the op_pop() function from earlier?
+
+
+def copy():
+
+    global op_stack
+
+    # Get the top value of the stack which represents the amount of things to copy
+    elements_to_copy = op_pop()
+
+    # Reverse this so you can traverse from the back
+    new_array = list(reversed(op_stack))[:elements_to_copy]
+
+    # The array of items to copy will be reverse order of what we want, so reverse it and push each value
+    for value in list(reversed(new_array)):
+        op_push(value)
+
+
+def clear():
+
+    global op_stack
+
+    # Just clear everything
+    op_stack.clear()
+
+
+def stack():
+
+    global op_stack
+
+    print("TOP")
+    print("~~~")
+
+    for item in list(reversed(op_stack)):
+        print(item)
+
+    print("~~~")
+    print("BOTTOM")
 
 # --------------------------- 20% -------------------------------------
 # Define the dictionary manipulation operators: psDict, begin, end, psDef
