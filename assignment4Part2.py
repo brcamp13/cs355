1f61e4e35f34ee165cfcd519ac84471e7960eac2
Brandon.Campbell <brcamp13@yahoo.com>
2018-11-06 14:01:40 -0800

    Some more minimal progress

--- assignment4Part2.py
+++ assignment4Part2.py
@@ -9,3 +9,14 @@ import re
 def tokenize(s):
  return re.findall("/?[a-zA-Z][a-zA-Z0-9_]*|[[][a-zA-Z0-9_\s!][a-zA-Z0-9_\s!]*[]]|[-]?[0-9]+|[}{]+|%.*|[^ \t\n]", s)
 
+ # Do all of the parsing stuff (She gives the necessary code for parsing)
+ 
+ def interpretSPS(code): 
+     pass
+
+def interpreter(s):
+    pass
+
+
+if __name__ == '__main__':
+    interpreter()
